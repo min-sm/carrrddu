422cf6eb870d456d386dd9df1aba04c61f1e934c
Min Soe Moe <minsoemoe999@gmail.com>
2024-04-13 21:30:29 +0630

    wait for 2 min

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post("/result", async (req, res) => {
     });
 
     const page = await browser.newPage();
-    // await page.setDefaultNavigationTimeout(120_000);
+    await page.setDefaultNavigationTimeout(120_000);
     await page.goto(`${reveiwLink}`, {
       waitUntil: "load",
     });
